65e8b697f4291b9777f4a02e87bf721dda2de7be
Malgosia <m.kapitanczyk@gmail.com>
2014-05-14 15:46:23 +0200

    dodalem funkcje info

--- kalkulator.py
+++ kalkulator.py
@@ -1,4 +1,11 @@
+def get_info():
+    print('To jest prosty kalkulator')
+
+
 def dodawanie():
-    return a+ b
+    a = int(input('Wprowadź a: '))
+    b = int(input('Wprowadź b: '))   
+    print(a+b)
     
+get_info()
 dodawanie()
